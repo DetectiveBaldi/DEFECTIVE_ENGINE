d0cf5eee245785df2c633badbca11205b29cad50
DetectiveBaldi <86160807+DetectiveBaldi@users.noreply.github.com>
2024-08-11 12:11:12 -0400

    53

--- source/tools/formats/charts/PsychFormat.hx
+++ source/tools/formats/charts/PsychFormat.hx
@@ -59,7 +59,7 @@ class PsychFormat
             {
                 var note:PsychNote = section.sectionNotes[j];
 
-                output.notes.push({time: note.time, speed: 1.0, direction: note.direction % 4, lane: 1 - Math.floor((section.mustHitSection ? note.direction : (note.direction >= 4.0) ? note.direction - 4.0 : note.direction + 4.0) * 0.25), length: note.length});
+                output.notes.push({time: note.time, speed: 1.0, direction: note.direction % 4, lane: 1 - Math.floor((section.mustHitSection ? note.direction : (note.direction >= 4.0) ? note.direction - 4 : note.direction + 4) * 0.25), length: note.length});
             }
         }
 
