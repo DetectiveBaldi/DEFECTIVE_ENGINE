a816b79aba22b7e55371b1607bf10be7f27c0047
DetectiveBaldi <86160807+DetectiveBaldi@users.noreply.github.com>
2024-09-16 00:27:20 -0400

    105

--- source/objects/PerfTracker.hx
+++ source/objects/PerfTracker.hx
@@ -44,6 +44,6 @@ class PerfTracker extends TextField
         while (times[0] < now - 1)
             times.shift();
 
-        text = 'FPS: ${MathUtil.minInt(FlxG.updateFramerate, times.length)}\nMemory: ${FlxMath.roundDecimal(System.totalMemory / Math.pow(1024, 2), 2)} MB';
+        text = 'FPS: ${MathUtil.minInt(FlxG.updateFramerate, times.length)}\nRAM: ${FlxMath.roundDecimal(System.totalMemory / Math.pow(1024, 2), 2)} MB\nVRAM: ${FlxMath.roundDecimal(FlxG.stage.context3D.totalGPUMemory / Math.pow(1024, 2), 2)} MB';
     }
 }
