2c6e15947f411615002b7f6341f1cf28d42223b5
DetectiveBaldi <86160807+DetectiveBaldi@users.noreply.github.com>
2024-12-03 06:43:46 -0500

    321

--- source/util/MathUtil.hx
+++ source/util/MathUtil.hx
@@ -4,36 +4,6 @@ import flixel.math.FlxMath;
 
 class MathUtil
 {
-    public static function min(...floats:Float):Float
-    {
-        var output:Float = FlxMath.MAX_VALUE_FLOAT;
-
-        for (i in 0 ... floats.length)
-        {
-            var float:Float = floats[i];
-
-            if (float < output)
-                output = float;
-        }
-
-        return output;
-    }
-
-    public static function max(...floats:Float):Float
-    {
-        var output:Float = FlxMath.MIN_VALUE_FLOAT;
-
-        for (i in 0 ... floats.length)
-        {
-            var float:Float = floats[i];
-
-            if (float > output)
-                output = float;
-        }
-
-        return output;
-    }
-
     public static function minInt(...ints:Int):Int
     {
         var output:Int = FlxMath.MAX_VALUE_INT;
